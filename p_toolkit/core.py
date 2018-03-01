447cf6b8d8d08c7cdc47add32888dd68e891d3ed
Esteban Angel Mesa <estebanangelm@gmail.com>
2018-03-01 14:50:16 -0800

    Add functions definitions

---
+++ p_toolkit/core.py
@@ -0,0 +1,32 @@
+def p_methods():
+    pass
+
+def p_adjust():
+    """
+    This function executes an specific p-value adjustment method.
+
+    Args:
+        - data: a Pandas dataframe or a 1-d Numpy array.
+        - col: if the input is a dataframe, 
+
+    """
+    pass
+
+def p_plot():
+    pass
+
+def p_qq():
+    pass
+
+def p_matrix():
+    pass
+
+def p_summary():
+    pass
+
+
+
+import pytest
+
+def test_p_methods():
+    assert
---
+++ p_toolkit/test_p_toolkit.py
@@ -0,0 +1,29 @@
+def p_methods():
+    pass
+
+def p_adjust():
+    """
+    This function outputs 
+
+    """
+    pass
+
+def p_plot():
+    pass
+
+def p_qq():
+    pass
+
+def p_matrix():
+    pass
+
+def p_summary():
+    pass
+
+
+
+import pytest
+
+def test_p_methods():
+
+    assert
