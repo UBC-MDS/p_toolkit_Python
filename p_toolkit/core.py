f83db36db0e5c8ad8fab655704273f06e0b9106f
Esteban Angel Mesa <estebanangelm@gmail.com>
2018-03-01 15:02:14 -0800

    Add docstring for p_adjust function

--- p_toolkit/core.py
+++ p_toolkit/core.py
@@ -7,8 +7,16 @@ def p_adjust():
 
     Args:
         - data: a Pandas dataframe or a 1-d Numpy array.
-        - col: if the input is a dataframe, 
+        - col: if the input is a dataframe, col refers to the column name of the dataframe that has the p-values.
+        - alpha: significance level for both methods.
+        - method: method used for the adjustment ("bh" or "bonf")
 
+    Returns:
+        - data frame: data frame with the following columns:
+            raw_p_value: original p-values.
+            adjusted_p_value: p-values after the adjustment.
+            signficant: boolean values determining if each p-value is significant.
+            critical_value: it's the calculated critical value to compare with the cut-off.
     """
     pass
 
