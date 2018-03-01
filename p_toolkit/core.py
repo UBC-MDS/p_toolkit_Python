da7413bafd7a2a7840ccac04f37b476f3664d498
Esteban Angel Mesa <estebanangelm@gmail.com>
2018-03-01 15:13:20 -0800

    Add qq plot ffunction docstring

--- p_toolkit/core.py
+++ p_toolkit/core.py
@@ -34,6 +34,18 @@ def p_plot():
     pass
 
 def p_qq():
+    """
+    This function plots all the raw p-values and compares them with a theoretical uniform distribution using a
+    qq plot. This plot is created with a negative log scale, letting
+    us visualize all the p-values, independent of their small magnitudes. The p-values deviated from the diagonal line,
+    are the ones that are significant.
+
+    Args:
+        - ad_object: the Pandas dataframe output from the p_methods function.
+
+    Returns:
+        - plot: a matplotlib object with the qq plot. 
+    """
     pass
 
 def p_matrix():
