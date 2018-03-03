fd5a35257e428b8b5cae6abb2e2ee91f245a077e
Esteban Angel Mesa <estebanangelm@gmail.com>
2018-03-02 22:39:54 -0800

    Add p_bh_helper function docstring

--- p_toolkit/core.py
+++ p_toolkit/core.py
@@ -65,7 +65,17 @@ def p_qq():
     """
     pass
 
-def p_matrix():
+def p_bh_helper():
+    """
+    Applies Benjamini-Hochberg (BH) correction to the original p-values
+
+    Args:
+        - pvals (int): original p-value column index from existing input dataframe
+        - alpha (int): significance level or false discovery rate as a value between 0 and 1
+
+    Returns:
+        - Vector: returns the Benjamini-Hochberg (BH) adjusted p-value
+    """
     pass
 
 def p_summary():
