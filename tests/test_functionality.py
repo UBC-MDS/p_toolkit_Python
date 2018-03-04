185beabfc56b3a742b73f04890613b3172737cb8
Esteban Angel Mesa <estebanangelm@gmail.com>
2018-03-04 11:02:30 -0800

    Add plot tests docstring

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -9,10 +9,10 @@ import pandas as pd
 
 def test_p_bonnferoni_helper():
     ###basic tests
-    assert p_bonnferoni_helper([0.07])== [0.07], "a single value doesn't change under bonnferoni"
-    assert p_bonnferoni_helper([0.07,0.2])== [0.14,0.4], "p_bonnferoni_helper((0.07,0.2))== (0.14,0.4)"
-    assert p_bonnferoni_helper([0.2,0.07])== [0.4,0.14], "p_bonnferoni_helper((0.2,0.07))== (0.4,0.14)"
-    assert p_bonnferoni_helper([0.01, 0.02, 0.03])==[0.03, 0.06,0.09], "p_bonnferoni_helper((0.01, 0.02, 0.03))==(0.03, 0.06,0.09)"
+    assert p_bonnferoni_helper([0.07]) == [0.07], "a single value doesn't change under bonnferoni"
+    assert p_bonnferoni_helper([0.07,0.2]) == [0.14,0.4], "p_bonnferoni_helper((0.07,0.2))== (0.14,0.4)"
+    assert p_bonnferoni_helper([0.2,0.07]) == [0.4,0.14], "p_bonnferoni_helper((0.2,0.07))== (0.4,0.14)"
+    assert p_bonnferoni_helper([0.01, 0.02, 0.03]) == [0.03, 0.06,0.09], "p_bonnferoni_helper((0.01, 0.02, 0.03))==(0.03, 0.06,0.09)"
 
     ### all values are between 0 and 1
     with pytest.raises(ValueError):
@@ -24,17 +24,17 @@ def test_p_bonnferoni_helper():
           p_bonferoni_helper([.05,8])
 
     ###maximum return is 1
-    assert p_bonnferoni_helper([0.01, 0.7])==[0.02,1], "test_p_bonnferoni_helperhas max of 1"
+    assert p_bonnferoni_helper([0.01, 0.7]) == [0.02,1], "test_p_bonnferoni_helperhas max of 1"
 
 
 def test_p_bonn():
-     assert p_bh_helper([0.07])== [0.07], "a single value doesn't change under bh"
-     assert p_bh_helper([0.07,0.2])== [0.14,0.2], "p_bh_helper((0.07,0.2))== (0.14,0.2)"
-     assert p_bh_helper([0.2,0.07])== [0.2,0.14], "p_bh_helper((0.2,0.07))== (0.2,0.14)"
-     assert p_bh_helper([0.01, 0.02, 0.03])==[0.03, 0.03,0.03], "p_bh_helper((0.01, 0.02, 0.03))==(0.03, 0.03,0.03)"
-     assert p_bh_helper([0.02, 0.03, 0.01])==[0.03, 0.03,0.03], "p_bh_helper((0.02, 0.03, 0.01))==(0.03, 0.03,0.03)"
-     assert p_bh_helper([.02,.12,.24,.56,.6])==[0.1,0.6, 0.4,0.7, 0.6], "p_bh_helper((.02,.12,.24,.56,.6))==(0.1,0.6, 0.4,0.7, 0.6)"
-     assert p_bh_helper([0.04, 0.04, 0.04,0.08])==[0.16,0.16,0.16,0.08], "p_bh_helper treats rep[eated values correctly"
+     assert p_bh_helper([0.07]) == [0.07], "a single value doesn't change under bh"
+     assert p_bh_helper([0.07,0.2]) == [0.14,0.2], "p_bh_helper((0.07,0.2))== (0.14,0.2)"
+     assert p_bh_helper([0.2,0.07]) == [0.2,0.14], "p_bh_helper((0.2,0.07))== (0.2,0.14)"
+     assert p_bh_helper([0.01, 0.02, 0.03]) == [0.03, 0.03,0.03], "p_bh_helper((0.01, 0.02, 0.03))==(0.03, 0.03,0.03)"
+     assert p_bh_helper([0.02, 0.03, 0.01]) == [0.03, 0.03,0.03], "p_bh_helper((0.02, 0.03, 0.01))==(0.03, 0.03,0.03)"
+     assert p_bh_helper([.02,.12,.24,.56,.6]) == [0.1,0.6, 0.4,0.7, 0.6], "p_bh_helper((.02,.12,.24,.56,.6))==(0.1,0.6, 0.4,0.7, 0.6)"
+     assert p_bh_helper([0.04, 0.04, 0.04,0.08]) == [0.16,0.16,0.16,0.08], "p_bh_helper treats rep[eated values correctly"
 
      ### all valid probabilities
      with pytest.raises(ValueError):
@@ -51,49 +51,49 @@ def test_p_adjust():
     ##basic vector functionality"
     d = { "p_value": [0.07], "adjusted"= [0.07]}
     df = pd.DataFrame(data = d)
-    assert p_adjust(data =[0.07], method = "bonnferoni")== df, "p_adjust 1 value vector for bonnferoni"
-    assert p_adjust(data =[0.07], method = "bh")== df, "p_adjust single value vector for bh"
+    assert p_adjust(data =[0.07], method = "bonnferoni") == df, "p_adjust 1 value vector for bonnferoni"
+    assert p_adjust(data =[0.07], method = "bh") == df, "p_adjust single value vector for bh"
 
     d = { "p_value": [0.07,0.2], "adjusted"= [0.14,0.4]}
     df = pd.DataFrame(data = d)
-    assert p_adjust(data =[0.07,0.2], method = "bonnferoni")== df, "p_adjust 2 values vector for bonnferoni"
+    assert p_adjust(data =[0.07,0.2], method = "bonnferoni") == df, "p_adjust 2 values vector for bonnferoni"
 
     d = { "p_value": [0.07,0.2], "adjusted"= [0.14,0.2]}
     df = pd.DataFrame(data = d)
-    assert p_adjust(data =[0.07,0.2], method = "bh")== df, "p_adjust 2 values vector value for bh"
+    assert p_adjust(data =[0.07,0.2], method = "bh") == df, "p_adjust 2 values vector value for bh"
 
     ###basic dataframe fuinctionality
     d = {"test":["test 1"], "p_value": [0.05]}
     df = pd.DataFrame(data = d)
     ad = {"test":["test 1"], "p_value": [0.05],"adjusted"= [0.05]}
     adf=pd.DataFrame(data =ad)
-    assert p_adjust(data =df, method = "bonnferoni")==adf, "bonferroni single value df under p_adjust"
-    assert p_adjust(data =df, method = "bh")==adf, "bh single value df under p_adjust"
+    assert p_adjust(data =df, method = "bonnferoni") == adf, "bonferroni single value df under p_adjust"
+    assert p_adjust(data =df, method = "bh") == adf, "bh single value df under p_adjust"
 
     d = {"test":["test 1", "test 2"], "p_value": [0.07,0.2]}
     df = pd.DataFrame(data = d)
     ad = {"test":["test 1", "test 2"], "p_value": [0.07,0.2],"adjusted"= [0.14, 0.4]}
     adf=pd.DataFrame(data =ad)
-    assert p_adjust(data =df, method = "bonnferoni")==adf, "bonferroni 2 value df under p_adjust"
+    assert p_adjust(data =df, method = "bonnferoni") == adf, "bonferroni 2 value df under p_adjust"
 
     ad = {"test":["test 1", "test 2"], "p_value": [0.07,0.2],"adjusted"= [0.14, 0.2]}
     adf=pd.DataFrame(data =ad)
-    assert p_adjust(data =df, method = "bh")==adf, "bh 2 value df under p_adjust"
+    assert p_adjust(data =df, method = "bh") == adf, "bh 2 value df under p_adjust"
 
 
 def test_p_methods():
     ###basic vector fuinctionality
     d = { "p_value": [0.07], "Bonnferoni_critical_value"= [0.05],"Bonnferoni_reject" =[False],"BH_critical_value"= [0.05], "BH_reject"=[False] }
     df = pd.DataFrame(data = d)
-    assert p_methods(data =[0.07], alpha =0.05)== df, "p_methods 1 value vector, FALSE"
+    assert p_methods(data =[0.07], alpha =0.05) == df, "p_methods 1 value vector, FALSE"
 
     d = { "p_value": [0.01], "Bonnferoni_critical_value"= [0.05],"Bonnferoni_reject" =[True],"BH_critical_value"= [0.05], "BH_reject"=[True] }
     df = pd.DataFrame(data = d)
-    assert p_methods(data =[0.01], alpha =0.05)== df, "p_methods 1 value vector, TRUE"
+    assert p_methods(data =[0.01], alpha =0.05) == df, "p_methods 1 value vector, TRUE"
 
     d = {"Test":["test 1", "test 2"], "p_value": [0.01,0.03], "Bonnferoni_critical_value"= [0.025,0.025],"Bonnferoni_reject" =[True,False],"BH_critical_value"= [0.025,0.05], "BH_reject"=[True, True] }
     df = pd.DataFrame(data = d)
-    assert p_methods(data =[0.01,0.03], alpha =0.05)== df, "p_methods 2 values vactor "
+    assert p_methods(data =[0.01,0.03], alpha =0.05) == df, "p_methods 2 values vactor "
 
 
     ###dataframe tests
@@ -120,5 +120,10 @@ def test_p_methods():
 def test_p_qq():
     """
     The purpose of this test is evaluating if the matplotlib object created has the correct layers compared to the required
-    plot.
+    plot. The test will cover the same things we checked with R:
+
+    - The output is a matplotlib object.
+    - The axis labels are correct. In this case if the labels are "Observed -log(p)" and "Expected -log(p)".
+    - The chart type used is correct. In this case, if it is a scatter plot combined with a line.
+    - The series used for plotting are the correct ones. "theoretical_pvalues" and "theoretical_pvalues".
     """
