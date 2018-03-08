f54228e89c1f893ee7664d539eecb437a39dff3b
Veronique Mulholland <vero.mulholland@gmail.com>
2018-03-08 13:23:10 -0800

    create helper functions

--- p_toolkit/core.py
+++ p_toolkit/core.py
@@ -70,13 +70,34 @@ def p_bh_helper():
     Applies Benjamini-Hochberg (BH) correction to the original p-values
 
     Args:
-        - pvals (int): original p-value column index from existing input dataframe
+        - p_values (int): original p-value column index from existing input dataframe
         - alpha (int): significance level or false discovery rate as a value between 0 and 1
 
     Returns:
         - Vector: returns the Benjamini-Hochberg (BH) adjusted p-value
     """
-    pass
+	# initialize parameters
+	q = alpha
+	
+	# Sort p-values
+	p_values = np.sort(p_values)
+	n = len(p_values)
+	i = np.arange(1, n+1)
+	
+	# Adjusted p-values that must be below the significance level
+	adj_bh = p_values * n / rank	
+	
+	# Rank to handle equal p-values 
+	helper_df = pd.DataFrame(p_values)
+	rank = round(helper_df.rank(axis=0, method = 'min')[0])
+ 
+    bh_df = pd.DataFrame()
+	bh_df['index'] = i 
+	bh_df['rank'] = rank
+	bh_df['p_value'] = p_values
+	bh_df['adjusted_pval'] = adj_bh
+	
+	return bh_df
 
 def p_bonferroni_helper():
     """
@@ -89,4 +110,12 @@ def p_bonferroni_helper():
     Returns:
         - vector: returns the Bonferroni adjusted p-value
     """
-    pass
+	# vector output
+	adj_bonf = alpha / pvals
+	
+	# dataframe output
+	bonf_df = pd.DataFrame()
+	bonf_df['p_value'] = p_values
+	bonf_df['adjusted_pval'] = adj_bonf
+	
+	return bonf_df
