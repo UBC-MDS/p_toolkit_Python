a32952443ea2b791eb13e0576b90e2f04210092c
Esteban Angel Mesa <estebanangelm@gmail.com>
2018-03-11 11:37:52 -0700

    Additional debugging for pip

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     description='A toolkit for adjusting and visualizing p values.',
     url = ['https://github.com/UBC-MDS/p_toolkit_Python'],
     download_url = 'https://github.com/UBC-MDS/p_toolkit_Python/archive/3.0.3.tar.gz',
-    keywords = ['pvalues']
+    keywords = ['pvalues'],
 )
