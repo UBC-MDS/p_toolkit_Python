5cd39658d33f28843aff4face4a9aa182bd00dc4
Esteban Angel Mesa <estebanangelm@gmail.com>
2018-03-11 11:36:24 -0700

    Add setup

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version='v1.0',
     author='Amy Goldlist, Veronique Mulholland and Esteban Angel',
     packages=['p_toolkit'],
+    author_email= 'estebanangelm@gmail.com',
     license='MIT',
     description='A toolkit for adjusting and visualizing p values.',
     url = ['https://github.com/UBC-MDS/p_toolkit_Python'],
-    download_url = 'https://github.com/UBC-MDS/p_toolkit_Python/archive/3.0.2.tar.gz'
+    download_url = 'https://github.com/UBC-MDS/p_toolkit_Python/archive/3.0.3.tar.gz',
+    keywords = ['pvalues']
 )
