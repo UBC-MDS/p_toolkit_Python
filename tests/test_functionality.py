d3b88d0197a274cf9b6df8d6a80212ece8957277
Véronique <vero.mulholland@gmail.com>
2018-03-10 22:50:18 -0800

    edit test caases and p_adjust

--- tests/test_functionality.py
+++ tests/test_functionality.py
@@ -15,126 +15,44 @@ sys.path.insert(0, os.path.abspath("../p_toolkit"))
 
 from core import *
 
+
 ###Functionality tests
 
-# def test_p_bon_helper():
-#     """
-#     The purpose of this test is performing numerical tests on the outputs from the p_bonnferoni_helper function. We test
-#     different combinations of inputs and check their corresponding outputs:
-#
-#     - The first block tests the Bonferroni adjustment.
-#     - The second one handls the cases with one of the values higher than 1, impossible for p-values.
-#     - The third block checks how the maximum value of 1 is handled in the function.
-#     """
-#     ###basic tests
-#     assert p_bonferroni_helper([0.07]) == [0.07], "a single value doesn't change under bonnferoni"
-#     assert p_bonferroni_helper([0.07,0.2]) == [0.14,0.4], "p_bonnferoni_helper((0.07,0.2))== (0.14,0.4)"
-#     assert p_bonferroni_helper([0.2,0.07]) == [0.4,0.14], "p_bonnferoni_helper((0.2,0.07))== (0.4,0.14)"
-#     assert p_bonferroni_helper([0.01, 0.02, 0.03]) == [0.03, 0.06,0.09], "p_bonnferoni_helper((0.01, 0.02, 0.03))==(0.03, 0.06,0.09)"
-#
-#     ### all values are between 0 and 1
-#     with pytest.raises(ValueError):
-#           p_bonferroni_helper([-3])
-#           p_bonferroni_helper([-3, .05])
-#           p_bonferroni_helper([.05, -3])
-#           p_bonferroni_helper([8])
-#           p_bonferroni_helper([8, .05])
-#           p_bonferroni_helper([.05,8])
-#
-#     ###maximum return is 1
-#     assert p_bonferroni_helper([0.01, 0.7]) == [0.02,1], "test_p_bonnferoni_helper has max of 1"
+###p_adjust functionality
+def test_p_adjust():
+    """
+    The purpose of this test is evaluating the p_adjust with more real world data using Pandas dataframes under
+    different environments.
+    """
 
-	###formatting tests
-	# https://github.com/datalyze-solutions/pandas-qt/blob/master/tests/test_DataFrameModel.py
-	# https://data-lessons.github.io/library-python/03-data-types-and-format/
-	#assert not data.empty
-	#assert data is dataFrame
-	#assert pvals == dtype(int32)
-	# assert alpha == dtype(int32)
+    ##basic vector functionality"
+    d = {"p_value": [0.07], "adjusted": [0.07]}
+    df = pd.DataFrame(data=d)
+    df = df[["p_value", "adjusted"]]
+    assert df.equals(p_adjust(data=[0.07], method="bonf")), "p_adjust 1 values vector for bonferoni"
+    assert df.equals(p_adjust(data=[0.07], method="bh")), "p_adjust 1 values vector for bh"
 
 
-# def test_p_bh():
-#     """
-#     The purpose of this test is performing numerical tests on the outputs from the p_bh_helper function. We test
-#     different combinations of inputs and check their corresponding outputs:
-#
-#     - The first block tests the BH adjustment.
-#     - The second one handles the cases with one of the values higher than 1, impossible for p-values.
-#     """
-#     assert p_bh_helper([0.07]) == [0.07], "a single value doesn't change under bh"
-#     assert p_bh_helper([0.07,0.2]) == [0.14,0.2], "p_bh_helper((0.07,0.2))== (0.14,0.2)"
-#     assert p_bh_helper([0.2,0.07]) == [0.2,0.14], "p_bh_helper((0.2,0.07))== (0.2,0.14)"
-#     assert p_bh_helper([0.01, 0.02, 0.03]) == [0.03, 0.03,0.03], "p_bh_helper((0.01, 0.02, 0.03))==(0.03, 0.03,0.03)"
-#     assert p_bh_helper([0.02, 0.03, 0.01]) == [0.03, 0.03,0.03], "p_bh_helper((0.02, 0.03, 0.01))==(0.03, 0.03,0.03)"
-#     assert p_bh_helper([.02,.12,.24,.56,.6]) == [0.1,0.6, 0.4,0.7, 0.6], "p_bh_helper((.02,.12,.24,.56,.6))==(0.1,0.6, 0.4,0.7, 0.6)"
-#     assert p_bh_helper([0.04, 0.04, 0.04,0.08]) == [0.16,0.16,0.16,0.08], "p_bh_helper treats rep[eated values correctly"
-#
-#     ### all valid probabilities
-#     with pytest.raises(ValueError):
-#         p_bh_helper([-3])
-#         p_bh_helper([-3, .05])
-#         p_bh_helper([.05, -3])
-#         p_bh_helper([8])
-#         p_bh_helper([8, .05])
-#         p_bh_helper([.05,8])
-#
-# 	# ###formatting tests
-# 	# # https://github.com/datalyze-solutions/pandas-qt/blob/master/tests/test_DataFrameModel.py
-# 	# assert not data.empty
-# 	# assert data is dataFrame
-# 	# assert BH_significant == type(numpy.bool_)
-# 	# assert Bonf_significant == type(numpy.bool_)
-#     #
-# 	# ###formatting tests
-# 	# # https://github.com/datalyze-solutions/pandas-qt/blob/master/tests/test_DataFrameModel.py
-# 	# assert not data.empty
-# 	# assert data is dataFrame
-# 	# assert pvals == dtype(int32)
-# 	# assert alpha == dtype(int32)
+    d = {"p_value": [0.07, 0.2], "adjusted": [0.14, 0.4]}
+    df = pd.DataFrame(data=d)
+    df = df[["p_value", "adjusted"]]
+    assert df.equals(p_adjust(data=[0.07, 0.2], method="bonf")), "p_adjust 2 values vector for bonferoni"
 
-####p_adjust functionality
-# def test_p_adjust():
-#     """
-#     The purpose of this test is evaluating the p_adjust with more real world data using Pandas dataframes under
-#     different environments.
-#     """
-#     ##basic vector functionality"
-#     d = { "p_value": [0.07], "adjusted": [0.07]}
-#     df = pd.DataFrame(data = d)
-#     assert p_adjust(data =[0.07], method = "bonf") == df, "p_adjust 1 value vector for bonnferoni"
-#     assert p_adjust(data =[0.07], method = "bh") == df, "p_adjust single value vector for bh"
-#
-#     d = { "p_value": [0.07,0.2], "adjusted": [0.14,0.4]}
-#     df = pd.DataFrame(data = d)
-#     assert p_adjust(data =[0.07,0.2], method = "bonf") == df, "p_adjust 2 values vector for bonnferoni"
-#
-#     d = { "p_value": [0.07,0.2], "adjusted": [0.14,0.2]}
-#     df = pd.DataFrame(data = d)
-#     assert p_adjust(data =[0.07,0.2], method = "bh") == df, "p_adjust 2 values vector value for bh"
-#
-#     ###basic dataframe fuinctionality
-#     d = {"test":["test 1"], "p_value": [0.05]}
-#     df = pd.DataFrame(data = d)
-#     ad = {"test":["test 1"], "p_value": [0.05],"adjusted": [0.05]}
-#     adf = pd.DataFrame(data =ad)
-#     assert p_adjust(data =df, method = "bonf") == adf, "bonferroni single value df under p_adjust"
-#     assert p_adjust(data =df, method = "bh") == adf, "bh single value df under p_adjust"
-#
-#     d = {"test":["test 1", "test 2"], "p_value": [0.07,0.2]}
-#     df = pd.DataFrame(data = d)
-#     ad = {"test":["test 1", "test 2"], "p_value": [0.07,0.2],"adjusted": [0.14, 0.4]}
-#     adf = pd.DataFrame(data =ad)
-#     assert p_adjust(data =df, method = "bonf") == adf, "bonferroni 2 value df under p_adjust"
-#
-#     ad = {"test":["test 1", "test 2"], "p_value": [0.07,0.2],"adjusted": [0.14, 0.2]}
-#     adf = pd.DataFrame(data =ad)
-#     assert p_adjust(data =df, method = "bh") == adf, "bh 2 value df under p_adjust"
-#
-# 	# ###formatting tests
-# 	# # https://github.com/datalyze-solutions/pandas-qt/blob/master/tests/test_DataFrameModel.py
-# 	# assert not data.empty
-# 	# assert data is dataFrame
+    d = {"p_value": [0.07, 0.2], "adjusted": [0.14, 0.2]}
+    df = pd.DataFrame(data=d)
+    df = df[["p_value", "adjusted"]]
+    assert df.equals(p_adjust(data=[0.07, 0.2], method="bh")), "p_adjust 2 values vector value for bh"
 
+
+
+    # error string col index of dataframe contains character values
+    try:
+        err_str = {"p_value": ['str']}
+        p_adjust((err_str), 0, "bonf", 0.05)
+    except(TypeError):
+        assert True
+    else:
+        assert False
 
 
 def test_p_methods():
@@ -142,61 +60,73 @@ def test_p_methods():
     The purpose of this test is evaluating the p_adjust with more real world data using Pandas dataframes under
     different environments.
     """
-    d = { "p_value": [0.07], "bonf_value": [0.05],"bonf_significant" :[False],"bh_value": [0.05], "bh_significant":[False] }
-    df = pd.DataFrame(data = d)
-    df = df[['p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
-    test =  p_methods(data =[0.07], alpha =0.05)
-    test = test[['p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
+    d = {"p_value": [0.07], "bonf_value": [0.05], "bonf_significant": [False], "bh_value": [0.05],
+         "bh_significant": [False]}
+    df = pd.DataFrame(data=d)
+    df = df[['p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
+    test = p_methods(data=[0.07], alpha=0.05)
+    test = test[['p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
     assert test.equals(df), "p_methods 1 value vector, FALSE"
 
-    d = { "p_value": [0.01], "bonf_value": [0.05],"bonf_significant" :[True],"bh_value": [0.05], "bh_significant":[True] }
-    df = pd.DataFrame(data = d)
-    df = df[['p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
-    test =  p_methods(data =[0.01], alpha =0.05)
-    test = test[['p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
+    d = {"p_value": [0.01], "bonf_value": [0.05], "bonf_significant": [True], "bh_value": [0.05],
+         "bh_significant": [True]}
+    df = pd.DataFrame(data=d)
+    df = df[['p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
+    test = p_methods(data=[0.01], alpha=0.05)
+    test = test[['p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
     assert test.equals(df), "p_methods 1 value vector, TRUE"
 
-
-    d = {"Test":["test 1", "test 2"], "p_value": [0.01,0.03], "bonf_value": [0.025,0.025],"bonf_significant" :[True,False],"bh_value": [0.025,0.05], "bh_significant":[True, True] }
-    df = pd.DataFrame(data = d)
-    df = df[['p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
-    test =  p_methods(data =[0.01,0.03], alpha =0.05)
-    test = test[['p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
+    d = {"Test": ["test 1", "test 2"], "p_value": [0.01, 0.03], "bonf_value": [0.025, 0.025],
+         "bonf_significant": [True, False], "bh_value": [0.025, 0.05], "bh_significant": [True, True]}
+    df = pd.DataFrame(data=d)
+    df = df[['p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
+    test = p_methods(data=[0.01, 0.03], alpha=0.05)
+    test = test[['p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
     assert test.equals(df), "p_methods 2 values vector "
 
-
     ###dataframe tests
-    d = {"Test":["test 1"], "p_value": [0.01] }
-    df = pd.DataFrame(data = d)
-    ad = {"Test":["test 1"], "p_value": [0.01], "bonf_value": [0.05],"bonf_significant" :[True],"bh_value": [0.05], "bh_significant":[True] }
-    adf = pd.DataFrame(data = ad)
-    adf = adf[['Test','p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
-    test =  p_methods(data =df, pv_index = "p_value", alpha =0.05)
-    test = test[['Test','p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
+    d = {"Test": ["test 1"], "p_value": [0.01]}
+    df = pd.DataFrame(data=d)
+    ad = {"Test": ["test 1"], "p_value": [0.01], "bonf_value": [0.05], "bonf_significant": [True], "bh_value": [0.05],
+          "bh_significant": [True]}
+    adf = pd.DataFrame(data=ad)
+    adf = adf[['Test', 'p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
+    test = p_methods(data=df, pv_index="p_value", alpha=0.05)
+    test = test[['Test', 'p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
     assert test.equals(adf), "p_methods 2 values dataframe "
 
-    d = {"Test":["test 1"], "p_value": [0.1] }
-    df = pd.DataFrame(data = d)
-    ad = {"Test":["test 1"], "p_value": [0.1], "bonf_value": [0.05],"bonf_significant" :[False],"bh_value": [0.05], "bh_significant":[False] }
-    adf = pd.DataFrame(data = ad)
-    adf = adf[['Test','p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
-    test =  p_methods(data =df,pv_index = "p_value", alpha =0.05)
-    test = test[['Test','p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
+    d = {"Test": ["test 1"], "p_value": [0.1]}
+    df = pd.DataFrame(data=d)
+    ad = {"Test": ["test 1"], "p_value": [0.1], "bonf_value": [0.05], "bonf_significant": [False], "bh_value": [0.05],
+          "bh_significant": [False]}
+    adf = pd.DataFrame(data=ad)
+    adf = adf[['Test', 'p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
+    test = p_methods(data=df, pv_index="p_value", alpha=0.05)
+    test = test[['Test', 'p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
     assert test.equals(adf), "p_methods 2 values dataframe "
 
-    d = {"Test":["test 1", "test 2"], "p_value": [0.01,0.03] }
-    df = pd.DataFrame(data = d)
-    ad = {"Test":["test 1", "test 2"], "p_value": [0.01,0.03], "bonf_value": [0.025,0.025],"bonf_significant" :[True,False],"bh_value": [0.025,0.05], "bh_significant":[True, True] }
-    adf = pd.DataFrame(data = ad)
-    adf = adf[['Test','p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
-    test =  p_methods(data =df, pv_index = "p_value", alpha =0.05)
-    test = test[['Test','p_value','bh_value','bh_significant','bonf_value','bonf_significant']]
+    d = {"Test": ["test 1", "test 2"], "p_value": [0.01, 0.03]}
+    df = pd.DataFrame(data=d)
+    ad = {"Test": ["test 1", "test 2"], "p_value": [0.01, 0.03], "bonf_value": [0.025, 0.025],
+          "bonf_significant": [True, False], "bh_value": [0.025, 0.05], "bh_significant": [True, True]}
+    adf = pd.DataFrame(data=ad)
+    adf = adf[['Test', 'p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
+    test = p_methods(data=df, pv_index="p_value", alpha=0.05)
+    test = test[['Test', 'p_value', 'bh_value', 'bh_significant', 'bonf_value', 'bonf_significant']]
     assert test.equals(adf), "p_methods 2 values dataframe "
 
     # data is not empty
     with pytest.raises(TypeError):
         p_methods()
 
+    # error string col index of dataframe contains character values
+    try:
+        err_str = {"p_value": ['str']}
+        p_adjust((err_str), 0, "bonf", 0.05)
+    except(TypeError):
+        assert True
+    else:
+        assert False
 
 ###Plotting tests
 
@@ -216,6 +146,7 @@ def test_p_qq():
 
     ##fig = p_qq(X,y)
     ##assert type(fig) == matplotlib.figure.Figure, "the object includes a matplotlib figure "
+
 
 def test_p_plot():
     """
