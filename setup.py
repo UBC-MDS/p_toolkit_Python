a5ebdd9de9e84b1b797f8a938181d63ff51c7ca3
vmulholl <vero.mulholland@gmail.com>
2018-03-16 18:36:37 -0700

    test removing matplotlib

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     url = ['https://github.com/UBC-MDS/p_toolkit_Python'],
     download_url = 'https://github.com/UBC-MDS/p_toolkit_Python/archive/3.0.4.tar.gz',
     keywords = ['pvalues'],
-    install_requires=['numpy', 'pandas', 'matplotlib']
+    install_requires=['numpy', 'pandas']
     )
